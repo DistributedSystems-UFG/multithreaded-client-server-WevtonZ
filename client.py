4650054b5a70b5faac9421cb4e66f834a86a2d4b
WevtonZ <wevtonj@gmail.com>
2026-04-20 22:51:02 -0300

    modificando os codigos para atender os requisitos da atividade.

--- client.py
+++ client.py
@@ -1,23 +1,42 @@
 from socket  import *
 from constCS import * #-
 from datetime import datetime
-
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
+import threading
+import random
 
 # a = add, s = subtract, m = multiply, d = divide
 
-n1 = int(input("Primeiro número: "))
-n2 = int(input("Segundo número: "))
-op = input("Operação (a=add, s=subtract, m=multiply, d=divide): ")
+def requisicao():
+    n1 = random.randint(1,100)
+    n2 = random.randint(1,100)
+    op = random.choice(['a','s','m','d'])
 
-msg = (n1,n2,op) # mudança: ler as variáveis agora para depois montar a tupla e mandar a mensagem
+    msg = (n1,n2,op)
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((HOST, PORT)) # connect to server (block until accepted)
+
+        s.send(str.encode(str(msg)))
+        data = s.recv(1024)
+
+        s.close()
+    
+    except Exception as e:
+        print(f"Deu ruim aqui: {e}")
+
+
+req = 400
+lista_threads = [] # guardar as threads em algum lugar
 
 t1 = datetime.now()
-s.send(str.encode(str(msg)))  # send some data
-data = s.recv(1024)     # receive the response
+
+for i in range(req):
+    t = threading.Thread(target=requisicao)
+    lista_threads.append(t)
+    t.start()
+
+for t in lista_threads:
+    t.join()
+
 t2 = datetime.now()
-print(bytes.decode(data))
-print (data.decode())            # print the result
 print(t2-t1)
-s.close()               # close the connection
--- server.py
+++ server.py
@@ -17,31 +17,34 @@ def trata_cliente(conn, addr):
 		n1=tpl[0]
 		n2=tpl[1]
 		op=tpl[2]
-
+		# se eu entendi corretamente, com threads, eu nao preciso mais retornar as coisas na
+		# funcao. O trabalho de resolver as coisas fica com a thread. Então, eu uso a função para
+		# poder resolver uma thread em particular e depois enviar a partir daqui mesmo.
+		res = 0
 		if op == 'a':
-			return n1+n2
+			res = n1+n2
 		elif op == 's':
-			return n1-n2
+			res = n1-n2
 		elif op == 'm':
-			return n1*n2
+			res = n1*n2
 		else:
 			if n2 == 0:
-				return -1
-			return n1/n2
-	except:
-		print("deu errado")
+				res = -1
+			res = n1/n2
+
+		conn.send(str.encode(str(res)))
+
+	except Exception as e:
+		print(f"Simplesmente deu errado de algum jeito: {e}")
+
 	finally:
 		conn.close()
 		print("acabou a thread atual.")
 
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client
+s.listen(5)           #-
 while True:                # forever
-	res = trata_cliente(conn, addr)
-	data = conn.recv(1024)   # receive data from client
-	if not data: break	   # stop if client stopped
-
-conn.send(str.encode(str(res))) # return sent data plus an "*"
-conn.close()  	             # close the connection
+	(conn, addr) = s.accept()  # returns new socket and addr. client
+	thread_cliente = threading.Thread(target = trata_cliente, args = (conn,addr))
+	thread_cliente.start()
